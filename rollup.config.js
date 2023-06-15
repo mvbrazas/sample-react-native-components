7c20a5f8589924daef5a6eff8fd06ef04646e28d
Jasper <mvbrazas@gmail.com>
2023-06-15 15:39:47 +0800

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js', // Entry point of your library
+  output: {
+    file: 'dist/bundle.js', // Output file
+    // format: 'cjs', // CommonJS module format for React Native
+    // exports: 'named',
+  },
+  external: ['react', 'react-native'], // Exclude React and React Native from the bundle
+  plugins: [
+    resolve({
+      extensions: ['.js', '.jsx'], // Resolve .js and .jsx files
+    }),
+    babel({
+      exclude: 'node_modules/**', // Exclude node_modules
+      presets: ['@babel/preset-env', '@babel/preset-react'], // Use the necessary presets
+    }),
+    commonjs(),
+  ],
+};
---
+++ src/components/SampleComponent/SampleComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+const SampleComponent = () => {
+  return (
+    <TouchableOpacity>
+      <Text>Sample Component</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default SampleComponent;
